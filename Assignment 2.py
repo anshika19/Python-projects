847c30d9afd98f854e251e36eacd8dff2bd2828f
anshika19 <64967808+anshika19@users.noreply.github.com>
2020-05-07 19:25:42 +0530

    Added 'positive integers' code

---
+++ Assignment 2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+list1= [12,-7,5,64,-14]
+for x in list1:
+    if x>=0:
+        print(x)
+
+
+# In[4]:
+
+
+list2= [12,14,-95,3]
+for y in list2:
+    if y>=0:
+        print(y)
+
+
+# In[ ]:
+
+
+
+
