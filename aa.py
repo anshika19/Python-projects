0cebee3e99a6db84fccfc58e3f05e99cf021df9d
anshika19 <64967808+anshika19@users.noreply.github.com>
2020-05-08 17:02:20 +0530

    Added assignment on data structures

---
+++ aa.py
@@ -0,0 +1,25 @@
+#assigning elements to different lists:
+list1=[1,8,90,76,52]
+list2=[12,44,9,67,44]
+list1.append(55)
+print(list1)
+list2.append(2)
+print(list2)
+
+
+
+#Accessing elements from a tuple:
+mytuple= ("Apple","Banana","Cherry")
+print(mytuple[1])
+
+
+#Deleting different dictionary elements:
+mydict= {
+    "A":"Apple",
+    "B":"Ball",
+    "C":"Cat"
+}
+print(mydict)
+del mydict["B"]
+print(mydict)
+         
